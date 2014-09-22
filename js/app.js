1aed39753c5e872291d67d731cd4ce0c89cf00a2
geoyws <geoyws@gmail.com>
2014-09-22 13:07:45 +0800

    singing about nonsense

--- js/app.js
+++ js/app.js
@@ -1,8 +1,11 @@
 // Inits
+app.init.ie8check();
+app.ready(app.init.startup);
+
 $(function() {
   var socket = io();
   document.getElementById('#m').focus();
-  
+    
   $('form').submit(function(event) {
     event.preventDefault();
     socket.emit('chatmsg', $('#m').val());
@@ -15,8 +18,30 @@ $(function() {
 
 // Library
 var app = {
-  init: {},
-  nav: function(destination, animation) {},
-  popup: function() {},
+  init: {
+    ie8check: function () {
+      if (window.attachEvent && !window.addEventListener) {
+        alert('Your browser is not supported. You might run into problems whilst using this app.'); 
+      }
+    },
+    startup: function () {
+      // list all startup functions here to run
+    }
+  },
+  nav: function (destination, animation) {
+    //window.location.href = destination;
+  },
+  ready: function (func) {
+    document.onreadystatechange = function () {
+      var state = document.readyState;
+      if (state == 'interactive') {
+        // do this
+      } else if (state == 'complete') {
+        // do this
+        func();
+      }
+    }
+  },
+  popup: function () {}
 };
 
