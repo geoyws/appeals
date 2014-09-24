f2965034148e67a627bb29e31456b51d55833516
geoyws <geoyws@gmail.com>
2014-09-24 13:37:10 +0800

    alrighty, geolocation on messages

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
 // Inits
-app.init.ie8check();
-app.ready(app.init.startup);
+app.ready(function () {
+    app.check.ie8Check();
+    app.cnvs.listen();
+});
 
+/*
 $(function() {
   var socket = io();
   document.getElementById('#m').focus();
-    
+  
   $('form').submit(function(event) {
     event.preventDefault();
     socket.emit('chatmsg', $('#m').val());
@@ -15,33 +18,76 @@ $(function() {
     $('#messages').append($('<li>').text(msg));
   });
 });
+*/
 
 // Library
 var app = {
-  init: {
-    ie8check: function () {
-      if (window.attachEvent && !window.addEventListener) {
-        alert('Your browser is not supported. You might run into problems whilst using this app.'); 
-      }
+    cnvs: { // cnvs is short for conversations
+	listen: {
+	    send: function () { // attach a listener to listen for message sending in conversations
+		document.getElementById('conversations').addEventListener('submit', function (event) {
+		    event.preventDefault();
+		    var id = event.target.id;
+		    var timestamp = new Date().getTime();
+		    var loc; // location, remember to utilize the geolocation plugin for PhoneGap
+		    var geolocationSuccess = function (position) { // Remember to add in "Do you want to enable location services for conversations?"
+			geolocationData = {
+			    'latitude': position.coords.latitude,
+			    'longtitude': position.coords.longtitude,
+			    'altitude': position.coords.altitude,
+			    'accuracy': position.coords.accuracy,
+			    'altitudeAccuracy': position.coords.altitudeAccuracy,
+			    'heading': position.coords.heading,
+			    'speed': position.coords.speed,
+			    'timestamp': position.timestamp     
+			};
+		    };
+		    var geolocationError = function (error) {
+			alert(
+			    'Geolocation Error Code:' + error.code + '\n' +
+			    error.message
+			);
+		    };
+		    var geolocationOptions = {
+			maximumAge: 3000, timeout: 5000, enableHighAccuracy: true
+		    };
+		    navigator.geolocation.getCurrentPosition(geolocationSuccess, geolocationError, geolocationOptions);
+		    var msg = document.querySelector(id + ' .active').value;
+		    var data = {
+			'conversationID': id,
+			'timestamp': timestamp,
+			'geolocationData': geolocationData, // location and geolocation are both reserved words
+			'message': msg // plain text only, if we are to send media, we do it by other means
+		    };
+		    socket.emit('chatmsg', JSON.stringify(data));
+		    msg = ''; // to clear the chat send box
+		});
+	    }
+	}
     },
-    startup: function () {
-      // list all startup functions here to run
-    }
-  },
-  nav: function (destination, animation) {
-    //window.location.href = destination;
-  },
-  ready: function (func) {
-    document.onreadystatechange = function () {
-      var state = document.readyState;
-      if (state == 'interactive') {
-        // do this
-      } else if (state == 'complete') {
-        // do this
-        func();
-      }
-    }
-  },
-  popup: function () {}
+    check: {
+    // checks to be done at startup
+	ie8Check: function () {
+	    if (window.attachEvent && !window.addEventListener) {
+		alert('Your browser is not supported. You might run into problems whilst using this app.'); 
+	    }
+	}
+    },
+    nav: function (destination, animation) {
+	// this will be the main way the app navigates between 'pages' via AJAX
+	//window.location.href = destination;
+    },
+    ready: function (func) {
+	document.onreadystatechange = function () {
+	    var state = document.readyState;
+	    if (state == 'interactive') {
+		// do this
+	    } else if (state == 'complete') {
+		// do this
+		func();
+	    }
+	}
+    },
+    popup: function () {}
 };
 
